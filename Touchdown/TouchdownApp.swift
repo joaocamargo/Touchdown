98f1c4d21b1649746a027dfc2fc1f0ba6198db5e
joao <joao.camargo@gmail.com>
2021-08-04 22:31:03 -0300

    first commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by joao camargo on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
